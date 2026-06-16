c368c633dd5bccc8badbdd2c24b201228cf1ce87
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2026-06-16 18:46:46 +0700

    hide qtajwid & qnahwu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,27 +26,27 @@ const services = [
       </svg>
     ),
   },
-  {
-    title: "qTajwid",
-    description:
-      "Pelajari tajwid interaktif dengan contoh suara, warna, dan latihan real-time.",
-    link: "/qtajwid",
-    icon: (
-      <svg
-        className="w-7 h-7"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
-        ></path>
-      </svg>
-    ),
-  },
+  // {
+  //   title: "qTajwid",
+  //   description:
+  //     "Pelajari tajwid interaktif dengan contoh suara, warna, dan latihan real-time.",
+  //   link: "/qtajwid",
+  //   icon: (
+  //     <svg
+  //       className="w-7 h-7"
+  //       fill="none"
+  //       stroke="currentColor"
+  //       viewBox="0 0 24 24"
+  //     >
+  //       <path
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //         strokeWidth="2"
+  //         d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
+  //       ></path>
+  //     </svg>
+  //   ),
+  // },
   {
     title: "qTahfidz",
     description:
@@ -68,27 +68,27 @@ const services = [
       </svg>
     ),
   },
-  {
-    title: "qNahwu",
-    description:
-      "Kuasai struktur bahasa Arab Al-Qur’an dengan pendekatan nahwu dan shorof yang mudah.",
-    link: "/qnahwu",
-    icon: (
-      <svg
-        className="w-7 h-7"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"
-        ></path>
-      </svg>
-    ),
-  },
+  // {
+  //   title: "qNahwu",
+  //   description:
+  //     "Kuasai struktur bahasa Arab Al-Qur’an dengan pendekatan nahwu dan shorof yang mudah.",
+  //   link: "/qnahwu",
+  //   icon: (
+  //     <svg
+  //       className="w-7 h-7"
+  //       fill="none"
+  //       stroke="currentColor"
+  //       viewBox="0 0 24 24"
+  //     >
+  //       <path
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //         strokeWidth="2"
+  //         d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"
+  //       ></path>
+  //     </svg>
+  //   ),
+  // },
   // {
   //   title: "qGames",
   //   description:
